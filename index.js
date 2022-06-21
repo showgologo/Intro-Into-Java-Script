9e6413471e2ac433b31b4de8b0f67405d9682e8d
showgologo <showgologo@localhost.localdomain>
2022-06-21 13:38:04 +0100

    First Java-Script Code

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let myName = ('<h1> My Name is Oluwasogo Emmanuel</h1> <br></br>');
+document.write(myName)
+
+let myHeight = ('<h2> I am 5ft 10inch tall</h2> <br></br>');
+document.write(myHeight)
+
+let myCountry = ('<h3> I am from Nigeria </h3> <br></br>');
+document.write(myCountry)
